aff9fb176c73e686412944c1c3a8a01bfab34cb6
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-08-22 02:44:29 +0300

    11

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
 
 const App = () => {
-    return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+    return (<div className='app-wrapper'>
+        <header className='header'>
+            <img src='https://73.img.avito.st/432x324/5252064473.jpg'/>
+        </header>
+        <nav className='nav'>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
+        </nav>
+        <div className='content'>
+            <div>
+                <img src='https://i.ytimg.com/vi/kdWwvE3wFaE/maxresdefault_live.jpg'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+    </div>);
 }
 
 export default App;
