2b50b8bd9251f04ab4e89a7c0a6d6e323cd0d49c
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-08-21 22:01:02 +0300

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
