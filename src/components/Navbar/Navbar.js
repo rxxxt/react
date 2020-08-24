66df43f5681923cd3f51cd697238f7660515a8f7
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-08-24 23:22:46 +0300

    19

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,19 +4,19 @@ import b from './Navbar.module.css';
 const Navbar = () => {
     return (<nav className={b.nav}>
         <div className={b.item}>
-            <a>Profile</a>
+            <a href='/profile'>Profile</a>
         </div>
         <div className={b.item}>
-            <a>Messages</a>
+            <a href='/dialogs'>Messages</a>
         </div>
         <div className={b.item}>
-            <a>News</a>
+            <a href='/news'>News</a>
         </div>
         <div className={b.item}>
-            <a>Music</a>
+            <a href='/music'>Music</a>
         </div>
         <div className={b.item}>
-            <a>Settings</a>
+            <a href='/settings'>Settings</a>
         </div>
     </nav>);
 }
