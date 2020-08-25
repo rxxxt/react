bc58f31d2cc4138ffb9404543662cbc9243f79f0
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-08-25 13:24:26 +0300

    21

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,10 +3,31 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hello!
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+                <div className={s.message}>
+                    Yo
+                </div>
+            </div>
         </div>
     );
 }
 
-export default  Dialogs;
+export default Dialogs;
