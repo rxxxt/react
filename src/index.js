dcedb8ae9cfea8a89e7bf8b076facf84fd9e1abb
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-08-29 00:44:01 +0300

    37

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/State";
+import store from "./redux/State";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {addPost, updateNewPostText} from "./redux/State"
 import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost}
-                     updateNewPostText={updateNewPostText}/>
+                <App state={state} addPost={store.addPost.bind(store)}
+                     updateNewPostText={store.updateNewPostText.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,72 +1,59 @@
 let store = {
-    __subscriber() {
-        console.log('no subscribers (observers)')
-    },
     _state: {
+        profilePage: {
+            posts: [
+                {id: 1, post: "Hi, how are you?", likeCounter: 15},
+                {id: 2, post: "My first post", likeCounter: 20},
+                {id: 3, post: "Hello World", likeCounter: 10},
+                {id: 4, post: "Yo yo yo yo yo", likeCounter: 50}
+            ],
+            newPostText: ''
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Dima"},
+                {id: 2, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Valera"},
+                {id: 3, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Sveta"}
+            ],
+            messages: [
+                {id: 1, message: "Hi!", answer: "Yo:)"},
+                {id: 2, message: "How are you?", answer: "I`m fine!"},
+                {id: 3, message: "Yo:)", answer: "Hello"}
+            ]
+        },
+        siteBar: {
+            friends: [
+                {id: 1, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Dima"},
+                {id: 2, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Valera"},
+                {id: 3, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Sveta"}
+            ]
+        }
+    },
+    getState()  {
+        return this._state;
+    },
+    _callSubscriber () {
+        console.log('State changed');
+    },
+    addPost () {
+        let newPost = {
+            id: 5,
+            post: this._state.profilePage.newPostText,
+            likeCounter: 0
+        };
 
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
     },
-    getState() {
-        return this.state;
+    updateNewPostText (newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
     },
-    subscribe(observer) {
-        this._subscriber = observer;
+    subscribe (observer) {
+        this._callSubscriber = observer;
     }
 }
 
-let rerenderEntireTree = () => {
-    console.log('State changed');
-}
-
-let state = {
-    profilePage: {
-        posts: [
-    {id: 1, post: "Hi, how are you?", likeCounter: 15},
-    {id: 2, post: "My first post", likeCounter: 20},
-    {id: 3, post: "Hello World", likeCounter: 10},
-    {id: 4, post: "Yo yo yo yo yo", likeCounter: 50}
-],
-        newPostText: ''
-    },
-    messagesPage: {
-        dialogs: [
-    {id: 1, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Dima"},
-    {id: 2, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Valera"},
-    {id: 3, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Sveta"}
-],
-        messages: [
-    {id: 1, message: "Hi!", answer: "Yo:)"},
-    {id: 2, message: "How are you?", answer: "I`m fine!"},
-    {id: 3, message: "Yo:)", answer: "Hello"}
-]
-    },
-    siteBar: {
-        friends: [
-            {id: 1, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Dima"},
-            {id: 2, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Valera"},
-            {id: 3, photo: "https://kbimages1-a.akamaihd.net/7cf95f7f-c08c-494b-a68f-c6023e2b3a1e/1200/1200/False/il-vocazzo-1.jpg", name: "Sveta"}
-        ]
-    }
-}
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        post: state.profilePage.newPostText,
-        likeCounter: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
