50850f4395a09985966d76e2740535aae17cf560
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-09-02 15:31:35 +0300

    61

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import a from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={a.header}>
-            <img src='https://mediamc.com.au/wp-content/uploads/2018/09/kisspng-social-media-marketing-youth-social-media-5ac29e4bc9dcf5.9923161815227039478268-1024x732-1.png'/>
+            <img
+                src='https://mediamc.com.au/wp-content/uploads/2018/09/kisspng-social-media-marketing-youth-social-media-5ac29e4bc9dcf5.9923161815227039478268-1024x732-1.png'/>
+            <div className={a.loginBlock}>
+                {props.isAuth ? props.login :
+                    <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     );
 }
