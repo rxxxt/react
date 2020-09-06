471f3a2a0958423d9c0213eb01a7ca5124f4545a
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-09-06 12:27:46 +0300

    71

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,17 +1,18 @@
 import React from "react";
 import c from "./ProfileInfo.module.css";
 import Preloader from "../../common/preloader/preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
         return <Preloader />
     }
-
     return (
         <div>
-            <div className={c.item}>
+            <ProfileStatus status="Hello my Friends"/>
+           {/* <div className={c.item}>
                 <img src='https://sun9-35.userapi.com/M9K-ZYqiyZlScM1k_sLis88eLKv-vHpCmrmgGg/JRr21OHal50.jpg'/>
-            </div>
+            </div>*/}
             <div className={c.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
                 ava + description
