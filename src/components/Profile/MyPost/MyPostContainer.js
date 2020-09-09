586960279d877b8264c47fbddb99a1586479e10b
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-09-09 19:58:52 +0300

    87

--- src/components/Profile/MyPost/MyPostContainer.js
+++ src/components/Profile/MyPost/MyPostContainer.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {addPost} from '../../../redux/profile-reducer';
-import MyPost from './MyPost';
-import {connect} from 'react-redux';
+import React from "react";
+import {addPost} from "../../../redux/profile-reducer";
+import MyPost from "./MyPost";
+import {connect} from "react-redux";
 
 let mapStateToProps = (state) => {
     return {
