fc0f9e001e7a28abced7bb92d74a2c908359beda
rxxxt <35171593+rxxxt@users.noreply.github.com>
2020-09-15 23:30:13 +0300

    Updates

---
+++ precache-manifest.b9b0fe9082d15adb229de37860c137d6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370e0ececb65acec06062b04ff299aca",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "5b11f7b72a94fd241d21",
+    "url": "/react/static/css/4.7b96bf1b.chunk.css"
+  },
+  {
+    "revision": "a1174c1ba167fe3af3ec",
+    "url": "/react/static/css/5.a35dd254.chunk.css"
+  },
+  {
+    "revision": "7cd756877c5fbd451c0c",
+    "url": "/react/static/css/6.a75ae4ae.chunk.css"
+  },
+  {
+    "revision": "2493f93a7d1342a3c744",
+    "url": "/react/static/css/7.870511a8.chunk.css"
+  },
+  {
+    "revision": "7f7efe51f2c528cf57bd",
+    "url": "/react/static/css/main.ffe5b296.chunk.css"
+  },
+  {
+    "revision": "1c7d4bc37d5ab3cb4816",
+    "url": "/react/static/js/0.b6d1ef93.chunk.js"
+  },
+  {
+    "revision": "74c4e38cfa71070c2842",
+    "url": "/react/static/js/3.0263b325.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/static/js/3.0263b325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b11f7b72a94fd241d21",
+    "url": "/react/static/js/4.f0963fff.chunk.js"
+  },
+  {
+    "revision": "a1174c1ba167fe3af3ec",
+    "url": "/react/static/js/5.037119db.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/react/static/js/5.037119db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd756877c5fbd451c0c",
+    "url": "/react/static/js/6.98b4e1a4.chunk.js"
+  },
+  {
+    "revision": "2493f93a7d1342a3c744",
+    "url": "/react/static/js/7.1369c977.chunk.js"
+  },
+  {
+    "revision": "7f7efe51f2c528cf57bd",
+    "url": "/react/static/js/main.7665b903.chunk.js"
+  },
+  {
+    "revision": "be8d960f0542401b1b28",
+    "url": "/react/static/js/runtime-main.dd78b9c1.js"
+  },
+  {
+    "revision": "e340c8b8ffd76828af4662da0413c606",
+    "url": "/react/static/media/preloader.e340c8b8.svg"
+  },
+  {
+    "revision": "25452ffd02a3379dc5a7961cb09d0efc",
+    "url": "/react/static/media/userPhoto.25452ffd.png"
+  }
+]);
